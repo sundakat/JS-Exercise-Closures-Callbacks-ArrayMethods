0a787d8e8dd3f348d471531b215e01be6a51ae10
Frances Hampton <bethhampton@Francess-MBP.attlocal.net>
2019-12-06 19:59:27 -0600

    getFullNames done

--- index.js
+++ index.js
@@ -181,8 +181,12 @@ function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS
  * @returns an array with all the runners' full names in the following format: "Smith, John".
  * The full names appear in the array in the same order the runners appear in the `runners` array.
 */
-function getFullNames(/* CODE HERE */) {
-  /* CODE HERE */
+function getFullNames(runners) {
+  let runnersArray = [];
+  runners.forEach(function(runner) {
+    runnersArray.push(`${runner.last_name}, ${runner.first_name}`);
+  });
+  return runnersArray;
 }
 
 /**
