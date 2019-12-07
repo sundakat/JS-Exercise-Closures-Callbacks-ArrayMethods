af149ea22874b2d32c7c42a5ba8c2792cfefb556
Frances Hampton <bethhampton@Francess-MBP.attlocal.net>
2019-12-06 21:24:10 -0600

    firstNamesAllCaps done

--- index.js
+++ index.js
@@ -201,8 +201,10 @@ function getFullNames(runners) {
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
-function firstNamesAllCaps(/* CODE HERE */) {
-  /* CODE HERE */
+function firstNamesAllCaps(runners) {
+  return runners.map(function(runners) {
+    return runners.first_name.toUpperCase();
+  });
 }
 
 /**
