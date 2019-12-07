522b532156ee3abd5b13422473e808be1f37c909
Frances Hampton <bethhampton@Francess-MBP.attlocal.net>
2019-12-07 17:05:18 -0600

    counterMakerWithLimit done

--- index.js
+++ index.js
@@ -288,8 +288,15 @@ function counterMaker() {
  * counter() // should return 0
  * etc
 */
-function counterMakerWithLimit(/* CODE HERE */) {
-  /* CODE HERE */
+function counterMakerWithLimit(limit) {
+  let count = 0;
+  function counter() {
+    if (count > limit) {
+      count = 0
+    }
+    return count++;
+  }
+  return counter
 }
 
 /////////////// END OF CHALLENGE ///////////////
