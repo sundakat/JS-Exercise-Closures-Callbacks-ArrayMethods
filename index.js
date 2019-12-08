75cdecfc0ce4a96ffcbfc5a617de94e34c5989b8
Frances Hampton <bethhampton@Francess-MBP.attlocal.net>
2019-12-08 01:28:50 -0600

    processDuplicateFree (Array is deduped)

--- index.js
+++ index.js
@@ -159,8 +159,10 @@ function processContains(item, list, callback) {
  * [2] Invoking `processDuplicateFree` passing `[1,1,2,2,3]` and `(arr) => arr.length`,
  * should return 3.
 */
-function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */) {
-  /* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */
+function processDuplicateFree(list, callback) {
+  return callback(list.filter((element, i, arr) => {
+    if (arr.indexOf(element) === i)return element;
+  }));
 }
 
 /////////////// HIGHER-ORDER ARRAY METHODS ///////////////
